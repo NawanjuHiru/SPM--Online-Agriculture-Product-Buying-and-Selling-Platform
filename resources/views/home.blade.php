a7ade026f744b654fcb22f19fa95fecfe6bea840
HirushaRukmal <hirusharukmal266@gmail.com>
2022-09-02 18:12:01 +0530

    Changes to Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,16 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{ URL::asset('public/css/home.css') }}" />
 </head>
 
 <body style="overflow-x:hidden">
 <!-- Nav bar-->
 <nav class="topnav-right" style="background-color: #6EBD6C; height:60px">
     <ul class="nav justify-content-end" style="">
+        <li class="nav-item" style="margin-right:800px;">
+            <h2 style="color: white;margin-top:10px">Agri Online</h2>
+        </li>
         <li class="nav-item">
             <a class="nav-link active" href="#" style="color: white;margin-top:10px">Home</a>
         </li>
@@ -28,51 +32,148 @@
         <li class="nav-item">
             <a class="nav-link" href="#" style="color: white;margin-top:10px">Contact Us</a>
         </li>
+        <li class="nav-item">
+            <button type="button" class="btn " style="margin-top: 12px; margin-right:20px; background-color:white; color:#6EBD6C;">
+            <i class="fas fa-user-alt" style="margin-right:2px; color:#6EBD6C"></i>
+            <span>Sign In</span>
+    </button>
+        </li>
     </ul>
 </nav>
 <!-- Nav bar-->
+
+<!--Home Content-->
 <div classname="" style="margin-left:100px">
     <img
-        src="Home.png"
+        src="http://unblast.com/wp-content/uploads/2020/05/Farming-Vector-Illustration.jpg"
         alt="Home Image"
-        style="float: right; width: 40%; height: 80%"
+        style="float: right; width: 46%; height: 50%;"
     />
-    <h1>
+    <h1 style="margin-top:20px;color:#6EBD6C;">
         A New Way to Invest<br/>
         in Agriculture
-    </h1>
+    </h1><br/>
     <span style="font-size: 20">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare
-      cursus<br/>
-      sed nunc eget dictum Sed ornare cursus sed nunc eget dictumd nunc eget<br/>
-      dictum Sed ornare cursus sed nunc eget dictum
+      Our portfolio of businesses caters to the entire Agri Value Chain and includes Seeds & Planting Materials, Crop Protection Compounds, Agri Equipment, Animal Health Products, Organic and Mineral Fertilizer, Environmental Science Products, Pre and Post Harvest Management Systems & Products, Green House and Protected Agriculture Structures as well as Water Management & Irrigation Systems. Our selection of services includes Pest Management and Landscaping.
     </span>
     <br/>
-    <button class="button" style="float: left; margin-top: 20px">
+    <button type="button" class="btn" style="float: left; margin-top: 20px; background-color:#6EBD6C; color: white;">
         <span>Get Started </span>
     </button>
     <img
-        src="leaves.png"
+        src="https://media.istockphoto.com/vectors/farmer-working-on-garden-vector-id1303644694?k=20&m=1303644694&s=612x612&w=0&h=xMAA4N-0vvzcZJy12DpGEiKltyHrv1rGIKcl0v6iOP4="
         alt="leaves Image"
         style="
         float: left;
-        width: 20%;
+        width: 30%;
         height: 40%;
-        margin-top: 120px;
-        margin-left: -150px;
+        margin-top: 60px;
+        margin-left: -260px;
       "
     />
-    <div style="padding-left: 300px; padding-top: 80px">
-        <h2>The Future of Agriculture <br/>investments</h2>
+    <div style="padding-left: 280px; padding-top: 100px">
+        <h2 style="color:#6EBD6C;">The Future of Agriculture <br/>investments</h2><br/>
         <span style="font-size: 20">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare
-        cursussed nunc eget dictum Sed ornare cursus sed nunc eget dictumd nunc
-        eget dictum Sed ornare cursus sed nunc eget dictum
+        Supporting Sustainable Greener Agriculture with Innovation, Quality Inputs and Extension Services.
       </span>
         <br/>
     </div>
+    <div class="" style="margin-top: 140px; margin-right: 40px; ">
+    <center><h1 class="" style="color:#6EBD6C;">Our Services</h1></center><br/>
+    <div class="row">
+    <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#6EBD6C;">Agricultural Products</h5>
+        <p class="card-text">We provide the client with the best products in agriculture.</p>
+        <a href="#" class="btn" style="background-color:#6EBD6C; color:white;">Shop Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#6EBD6C;">Agricultural Equipments</h5>
+        <p class="card-text">We provide the client with the best equipments in agriculture.</p>
+        <a href="#" class="btn" style="background-color:#6EBD6C; color:white;">Shop Now</a>
+      </div>
+    </div>
+  </div>
+</div><br/>
+<div class="row">
+    <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#6EBD6C;">Platform to sell your goods</h5>
+        <p class="card-text">We provide the client with the best platform to sell their agricultural products.</p>
+        <a href="#" class="btn" style="background-color:#6EBD6C; color:white;">Shop Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#6EBD6C;">Platform to buy your goods</h5>
+        <p class="card-text">We provide the client with the best platform to buys agricultural products.</p>
+        <a href="#" class="btn" style="background-color:#6EBD6C; color:white;">Shop Now</a>
+      </div>
+    </div>
+  </div>
 </div>
-
+    </div>
+    <div class="" style="margin-top: 100px; margin-right: 40px; ">
+        <div class="card text-bg-success mb-3" style="max-width: auto;">
+        <div class="card-header"><h2 class="" style="color:#6EBD6C;">About Us</h2></div>
+        <div class="card-body">
+        <img
+        src="https://thumbs.dreamstime.com/b/logotype-agriculture-logo-field-wheat-stylized-eco-logo-green-energy-fields-logotype-agriculture-logo-field-117795540.jpg"
+        alt="Home Image"
+        style="float: right; width: 25%; height: 25%; margin-right:80px"/>
+        <p class="card-text">Our system is an online Agriculture product buying and selling platform (E-Marketplace)<br/>
+        which the user can sell their products (Fruits, vegetables, seeds, fertilizer, and equipment <br/>
+        related to agriculture field) and users can buy any of products as they wish. <br/>The system is mainly focused on Sri Lankan agriculture sector.</p>
+        <p class="card-text"> From this website Farmers will be able to get their needed products for their plantation and agricultural activities<br/>
+        This website also support the buyers who need to buy the needed equipments and products for their purposes.<br/> Any Client can sell their products in this website. This will be very helpful for client involve in agricultural sector<br/> to fullfill their needs and wants.
+  </div>
+</div>
+<div class="" style="margin-top: 90px; margin-right: 40px; ">
+    <center><h1 class="" style="color:#6EBD6C;">What our clients say about us</h1></center><br/>
+    <div class="card-deck">
+  <div class="card" style="background-color:#F7F7F7;">
+    <center><img src="https://www.pngitem.com/pimgs/m/334-3344170_user-vector-user-flat-png-transparent-png.png" class="card-img-top" alt="..." style="height: 200px; width:200px;"></center>
+    <div class="card-body">
+      <h5 class="card-title">John Doe</h5>
+      <p class="card-text">Great service, efficient communication and a really easy way to get a mortgage with lots of help and support to get the right deal.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card" style="background-color:#F7F7F7;">
+    <center><img src="https://png.pngitem.com/pimgs/s/130-1300253_female-user-icon-png-download-user-image-color.png" class="card-img-top" alt="..." style="height: 200px; width:200px;"></center>
+    <div class="card-body">
+      <h5 class="card-title">Ann Mary</h5>
+      <p class="card-text">We were provided excellent customer service. All responses were prompt and professional. Clearly, the upfront statement on our policy stated it was limited which told us that our preliminary research was not well thought out.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card"  style="background-color:#F7F7F7;">
+    <center><img src="https://png.pngitem.com/pimgs/s/22-220721_circled-user-male-type-user-colorful-icon-png.png" class="card-img-top" alt="..." style="height: 200px; width:200px;"></center>
+    <div class="card-body">
+      <h5 class="card-title">Peter Stuward</h5>
+      <p class="card-text">Excellent customer service! Whenever I needed something they were there for me.<br/>Thank you guys.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<!--Home Content-->
 
 <!-- Footer -->
 <footer class="text-white"
@@ -82,19 +183,16 @@
         <!--Grid row-->
         <div class="row" style>
             <!--Grid column-->
-            <div class="align-left" style="margin-left: -300px">
-                <h5 class="text-uppercase">Agri Online</h5>
-
+            <div class="align-left" style="margin-left: -auto">
+                <h3 class="">Agri Online</h3>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-                    aliquam voluptatem veniam, est atque cumque eum delectus sint!
+                    Supporting Sustainable Greener Agriculture with Innovation,<br/> Quality Inputs and Extension Services.
                 </p>
             </div>
             <!--Grid column-->
 
             <!--Grid column-->
-            <div class="float-end" style="margin-left: 900px; margin-top: -120px;">
+            <div class="float-end" style="margin-left: 900px; margin-top: -100px;">
                 <h5 class="text-uppercase">Navigate</h5>
 
                 <ul class="list-unstyled mb-0">
@@ -113,7 +211,7 @@
                 </ul>
             </div>
             <!--Grid column-->
-            <div class="" style="margin-left: -310px; margin-top: 0px;">
+            <div class="" style="margin-left: -10px; margin-top: -20px;">
                 <i class="fab fa-facebook" style="margin-left: 10px;"></i>
                 <i class="fab fa-linkedin" style="margin-left: 10px;"></i>
                 <i class="fab fa-instagram" style="margin-left: 10px;"></i>
